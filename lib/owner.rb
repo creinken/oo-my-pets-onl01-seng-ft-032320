9383f065e987e5f258bc694554226ee2715b1f3e
Christopher Reinken <christopherreinken@gmail.com>
2020-04-06 17:45:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,12 @@
 class Owner
+  
   attr_reader :name, :species
+  @@all = []
   
   def initialize(name, species)
     @name = name
     @species = species
+    @@all << self
   end
   
   def say_species
